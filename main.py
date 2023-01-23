b257773bd57ca590d4e1aa9a2eae37732ce12e72
altrono <hbotema16@gmail.com>
2023-01-23 10:47:46 +0200

    Added snake body

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from turtle import Screen, Turtle
 
+# Screen setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Anaconda Snake Game')
 
 
-
-
-
-
-
-
-
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+my_snake_list = []
+for position in starting_position:
+    new_turtle = Turtle("square")
+    new_turtle.color('white')
+    new_turtle.goto(position)
+    my_snake_list.append(new_turtle)
 
 
 
